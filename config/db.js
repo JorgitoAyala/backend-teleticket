992a196ef3759a03940d749ae9169fe846163351
Jorge Ayala <74685456+JorgitoAyala@users.noreply.github.com>
2022-12-03 04:16:04 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ require("dotenv").config({ path: "variables.env" });
 
 const connnectDB = async () => {
   try {
-    await mongoose.connect(process.env.DB_MONGO, {
+    mongoose.connect(process.env.DB_MONGO, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
